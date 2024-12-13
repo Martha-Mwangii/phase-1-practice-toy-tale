5f96cdda2f21f1e6460c4b72bc0812558d76904c
Martha Wangui Mwangi <marthawm7252@gmail.com>
2024-12-13 12:58:29 +0300

    phase-1-practice-toy-tale

--- src/index.js
+++ src/index.js
@@ -13,3 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const toyCollect = document.getElementById('toy-collection')
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('http://localhost:3000/toys')
+  .then((res) => res.json())
+  .then((toyData) => toyData.forEach(toy => {
+    
+      //card container
+    const toyCard = document.createElement('div')
+    toyCard.className = 'card'
+    toyCollect.append(toyCard)
+    //toyname
+    const toyName = document.createElement('h2')
+    toyName.textContent = toy.name
+    toyCard.append(toyName)
+    //
+    const imgTag = document.createElement('img')
+    imgTag.className = 'toy-avatar'
+      //inserting img from object to imgTag
+    imgTag.src = toy.image
+    toyCard.append(imgTag)
+    console.log(toyCard)
+    //Adding Likes
+    const toyLikes = document.createElement('p')
+    toyLikes.textContent = toy.likes
+    toyCard.append(toyLikes + "Likes")
+    
+    const toyLikeButton = document.createElement('button')
+    toyLikeButton.textContent = 'Like ❤️'
+    toyLikeButton.class = 'like-btn'
+    toyLikeButton.id = '[toy_id]'
+    toyCard.append(toyLikeButton) 
+  }))
+});
